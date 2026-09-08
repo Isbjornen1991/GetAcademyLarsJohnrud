9012a210a06f60b7a778a9a54bfa20d1cdc34248
Isbjornen1991 <lars_johnsrud@outlook.com>
2026-09-08 15:29:27 +0200

    Did more unit testing tasks

--- Week5/161/161Bscripts.js
+++ Week5/161/161Bscripts.js
@@ -1,32 +1,58 @@
 //Model
-
 let inputEmail = "";
+let outputArea = document.getElementById("output");
+let isValid = false;
 
 //View
 function updateView() {
   app.innerHTML = /*HTML*/ `
-
+    <div>
       <label>Enter your example.com email:</label>
       <input
         type="text"
         value="${inputEmail}"
         oninput="betterTestEmail(this.value)">
-
-        <button onclick="betterTestEmail(inputEmail)">Test The Email</button>
+    </div>
+    <div>
+      <div id="output">${isValid}</div>
+    </div>
         `;
 }
 
-function testEmail(textInput) {
-  for (let i = 0; i < textInput.length; i++) {
-    if (textInput[i] === "@") {
-      console.log(true);
-      return true;
-    }
-  }
-  console.log(false);
-  return false;
-}
+//Controller
+
+//Old, works, but is just worse
+
+// function testEmail(textInput) {
+//   for (let i = 0; i < textInput.length; i++) {
+//     if (textInput[i] === "@") {
+//       console.log(true);
+//       return true;
+//     }
+//   }
+//   console.log(false);
+//   return false;
+// }
 
 function betterTestEmail(textInput) {
-  return textInput.includes("@");
+  inputEmail = textInput;
+  isValid = textInput.includes("@");
+  checkForPeriod(textInput);
+
+  console.log("Is email valid?", isValid);
+
+  document.getElementById("output").innerText = isValid;
+
+  return isValid;
+}
+
+function checkForPeriod(textInput) {
+  const splitArray = textInput.split("@");
+  console.log(splitArray[0]);
+
+  if (splitArray[0]?.includes(".") && splitArray[1]?.includes(".")) {
+    console.log(true);
+    return true;
+  }
+  return false;
 }
